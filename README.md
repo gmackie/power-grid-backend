2bad8a52b5c84e138375589c1e6f2774dd4fc857
gmackie <graham.mackie@gmail.com>
2021-05-24 16:21:14 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Power Grid - Backend
+This is the repo for the RESTful backend to support an online version of the Board Game "Power Grid" (add link).
+
+
+more info will go here later....
---
+++ types.ts
@@ -0,0 +1,76 @@
+
+enum Resource {
+  COAL = "COAL",
+  OIL = "OIL",
+  HYBRID = "HYBRID",
+  TRASH = "TRASH",
+  URANIUM = "URANIUM",
+  GREEN = "GREEN",
+};
+
+interface PowerPlant {
+  initialCost: number;
+  resourcesRequired: number;
+  resourseType: Resource;
+  housesPowered: number;
+}
+
+interface ResourceState {
+  resourceType: Resource;
+  available: {
+    cost: number;
+    quantity: number;
+  }[]
+}
+
+interface GameState {
+  players: PlayerState[];
+  market: PowerPlant[];
+  deck: PowerPlant[];
+  discard: PowerPlant[];
+  gamePhase: number;
+  roundStep: number;
+  cities: CityState[];
+  resources: ResourceState[];
+}
+
+interface BidState {
+  currentBidder: string;
+  remainingBidders: string[];
+  eligibleBidders: string[];
+  currentBid: number;
+  powerPlant: PowerPlant;
+}
+
+interface PowerPlantState {
+  powerPlant: PowerPlant;
+  currentResources: {
+    resourceType: Resource;
+    quantity: number;
+  }[];
+}
+
+interface PlayerState {
+  id: string;
+  money: number;
+  powerPlants: PowerPlant[];
+  cities: string[];
+}
+
+interface CityState {
+  name: string;
+  region: number;
+  location: {
+    x: number;
+    y: number;
+  };
+  connections: {
+    name: string;
+    cost: number;
+    location: {
+      x: number;
+      y: number;
+    };
+  }[];
+  houses: string[];
+}
